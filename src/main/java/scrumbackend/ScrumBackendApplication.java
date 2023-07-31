ec4435b70036356e7ed7cec006e051fa1d353bb9
Keanu Reaño <reanokeanu@gmail.com>
2023-07-31 09:04:31 +0800

    init boilerplate

---
+++ src/main/java/scrumbackend/ScrumBackendApplication.java
@@ -0,0 +1,13 @@
+package scrumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scrumbackend/ScrumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package scrumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
