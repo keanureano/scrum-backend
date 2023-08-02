2de3a11b2758a3249372d8a3a2614fa0cb5d1fa2
Kutz <kutzruzzel.sumaya60@gmail.com>
2023-08-02 11:42:27 +0800

    added findBy team

--- src/main/java/scrumbackend/team/TeamRepository.java
+++ src/main/java/scrumbackend/team/TeamRepository.java
@@ -1,7 +1,11 @@
 package scrumbackend.team;
 
+import java.util.Optional;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {
- 
+public interface TeamRepository extends JpaRepository<Team, Long> { 
+        Optional<Team> findById(Long id);
+        List<Team> findByNameContainsIgnoreCase(String name);
 }
