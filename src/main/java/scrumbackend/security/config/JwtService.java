b166d947f8237dec6c26a3834b9b265130687edb
Keanu Reaño <reanokeanu@gmail.com>
2023-08-09 11:29:10 +0800

    [gac] feat(security): add JWT token generation and extraction Adds functionality for generating and extracting JWT tokens in the security configuration. This commit introduces the following changes: Imports io.jsonwebtoken.SignatureAlgorithm, java.util.Date, java.util.HashMap, and java.util.Map. Adds methods generateToken(UserDetails userDetails) and generateToken(Map<String, Object> extraClaims, UserDetails userDetails) to the JwtService class. generateToken methods create JWT tokens with specified claims and expiration time. Updates the extractUsername method to use the extractClaim method with Claims::getSubject. Enhances code readability by using method chaining and appropriate indentation. Co-authored-by: [Author Name] author@email.com Co-authored-by: [Co-author Name] coauthor@email.com

--- src/main/java/scrumbackend/security/config/JwtService.java
+++ src/main/java/scrumbackend/security/config/JwtService.java
@@ -2,11 +2,16 @@ package scrumbackend.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +21,7 @@ public class JwtService {
   private String jwtSecretKey;
 
   public String extractUsername(String jwtToken) {
-    return null;
+    return extractClaim(jwtToken, Claims::getSubject);
   }
 
   public <T> T extractClaim(
@@ -25,6 +30,24 @@ public class JwtService {
   ) {
     final Claims claims = extractAllClaims(jwtToken);
     return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(
+    Map<String, Object> extraClaims,
+    UserDetails userDetails
+  ) {
+    return Jwts
+      .builder()
+      .setClaims(extraClaims)
+      .setSubject(userDetails.getUsername())
+      .setIssuedAt(new Date(System.currentTimeMillis()))
+      .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+      .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+      .compact();
   }
 
   private Claims extractAllClaims(String jwtToken) {
