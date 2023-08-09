a527ee8e53448714649bde3e7ed4721b90a5ac19
Keanu Reaño <reanokeanu@gmail.com>
2023-08-09 11:18:52 +0800

    [gac] feat(security): add JWT token parsing logic Add logic to parse JWT tokens in the JwtService class. The extractAllClaims method is implemented to extract claims from a given JWT token using the provided secret key. The getSigningKey method is used to obtain the signing key from the jwtSecretKey property defined in the application properties file. This change enhances the JWT token handling in the security module and improves the extraction of user claims from JWT tokens. Refs: #123

--- src/main/java/scrumbackend/security/config/JwtService.java
+++ src/main/java/scrumbackend/security/config/JwtService.java
@@ -1,11 +1,34 @@
 package scrumbackend.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+  @Value("${jwt.secretKey}")
+  private String jwtSecretKey;
+
   public String extractUsername(String jwtToken) {
     return null;
+  }
+
+  private Claims extractAllClaims(String jwtToken) {
+    return Jwts
+      .parserBuilder()
+      .setSigningKey(getSigningKey())
+      .build()
+      .parseClaimsJws(jwtToken)
+      .getBody();
+  }
+
+  private Key getSigningKey() {
+    byte[] jwtSecretKeyBytes = Decoders.BASE64.decode(jwtSecretKey);
+    return Keys.hmacShaKeyFor(jwtSecretKeyBytes);
   }
 }
