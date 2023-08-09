238c846e11627d3b64d60cdf2a6dc6353417a427
Keanu Reaño <reanokeanu@gmail.com>
2023-08-09 12:55:58 +0800

    [gac] feat: Add API endpoints for user registration and login     This commit adds new API endpoints for user registration and login. The registration endpoint allows clients to send a POST request to http://localhost:8080/api/auth/register with a JSON payload containing email, name, and password fields. Similarly, the login endpoint is accessible via POST request to http://localhost:8080/api/auth/login with a JSON payload containing email and password fields. These endpoints are secured using JWT token-based authentication, where the token is obtained from the Authorization header with the Bearer scheme. This implementation enhances the user authentication and management capabilities of the application.

--- src/main/java/scrumbackend/ScrumBackendApplication.java
+++ src/main/java/scrumbackend/ScrumBackendApplication.java
@@ -1,109 +1,14 @@
 package scrumbackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import scrumbackend.issue.Issue;
-import scrumbackend.issue.IssueRepository;
-import scrumbackend.task.Task;
-import scrumbackend.task.TaskRepository;
-import scrumbackend.team.Team;
-import scrumbackend.team.TeamRepository;
-import scrumbackend.user.Role;
-import scrumbackend.user.User;
-import scrumbackend.user.UserRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class ScrumBackendApplication implements CommandLineRunner {
+public class ScrumBackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ScrumBackendApplication.class, args);
-  }
-
-  @Autowired
-  IssueRepository issueRepository;
-
-  @Autowired
-  TaskRepository taskRepository;
-
-  @Autowired
-  UserRepository userRepository;
-
-  @Autowired
-  TeamRepository teamRepository;
-
-  @Override
-  public void run(String... args) throws Exception {
-    Issue issue = Issue
-      .builder()
-      .issuesToday("Issue 1")
-      .issuesYesterday("Issue 1")
-      .isResolved(true)
-      .build();
-    issueRepository.save(issue);
-
-    Issue issue2 = Issue
-      .builder()
-      .issuesToday("Issue 2")
-      .issuesYesterday("Issue 2")
-      .build();
-    issueRepository.save(issue2);
-
-    Team team = Team.builder().name("Team 1").build();
-    teamRepository.save(team);
-
-    Team team2 = Team.builder().name("Team 2").build();
-    teamRepository.save(team2);
-
-    User user = User
-      .builder()
-      .name("Franz")
-      .email("420@gmail.com")
-      .password("password")
-      .role(Role.ADMIN)
-      .team(team)
-      .build();
-    userRepository.save(user);
-
-    User user2 = User
-      .builder()
-      .name("Lariba")
-      .email("islife@gmail.com")
-      .password("password")
-      .role(Role.USER)
-      .team(team2)
-      .build();
-    userRepository.save(user2);
-
-    scrumbackend.task.Task task = scrumbackend.task.Task
-      .builder()
-      .tasksToday("Task Today 1")
-      .tasksYesterday("Task Yesterday 1")
-      .impediments("Impediment 1")
-      .user(user)
-      .build();
-    taskRepository.save(task);
-
-    scrumbackend.task.Task task2 = Task
-      .builder()
-      .tasksToday("Task Today 2")
-      .tasksYesterday("Task Yesterday 2")
-      .impediments("Impediment 2")
-      .user(user2)
-      .build();
-    taskRepository.save(task2);
-
-    scrumbackend.task.Task task3 = Task
-      .builder()
-      .tasksToday("Task Today 2")
-      .tasksYesterday("Task Yesterday 2")
-      .impediments("Impediment 2")
-      .user(user2)
-      .isResolved(true)
-      .build();
-    taskRepository.save(task3);
   }
 }
