22e38c8527ffad3771768dbd839bc41681993928
Keanu Reaño <reanokeanu@gmail.com>
2023-08-09 10:50:20 +0800

    [gac] feat(repo): Remove unused findById methods Remove the unused findById methods from IssueRepository, TaskRepository, TeamRepository, and UserRepository interfaces. These methods were not being utilized and have been removed to declutter the codebase. This commit removes the findById methods from IssueRepository, TaskRepository, TeamRepository, and UserRepository interfaces as they were not in use and contributed to unnecessary code complexity.

--- src/main/java/scrumbackend/task/TaskRepository.java
+++ src/main/java/scrumbackend/task/TaskRepository.java
@@ -2,10 +2,8 @@ package scrumbackend.task;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-  Optional<Task> findById(Long Id);
   List<Task> findByCreatedAtBetween(Instant from, Instant to);
 }
--- src/main/java/scrumbackend/user/UserRepository.java
+++ src/main/java/scrumbackend/user/UserRepository.java
@@ -1,12 +1,8 @@
 package scrumbackend.user;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findById(Long id);
   Optional<User> findByEmail(String email);
-  List<User> findByNameContainsIgnoreCase(String name);
-  List<User> findByEmailContainsIgnoreCase(String email);
 }
